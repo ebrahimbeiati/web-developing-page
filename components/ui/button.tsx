f51f33f13f850b94ac413ff11ee14d7a1afeb003
Ebrahim Beiati-Asl <ebrahimbeiaty@gmail.com>
2024-05-24 00:14:19 +0100

    add title

---
+++ components/ui/button.tsx
@@ -0,0 +1,22 @@
+// Button Component
+import classNames from "classnames";
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: "outline";
+  size?: "icon";
+}
+
+export function Button({ variant, size, children, ...props }: ButtonProps) {
+  const baseStyles = "px-4 py-2 rounded";
+  const variantStyles = variant === "outline" ? "border border-gray-300" : "";
+  const sizeStyles = size === "icon" ? "p-2" : "";
+
+  return (
+    <button
+      className={classNames(baseStyles, variantStyles, sizeStyles)}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+}
---
+++ components/ui/dropdown-menu.tsx
@@ -0,0 +1,55 @@
+import classNames from "classnames";
+interface DropdownMenuProps {
+  children: React.ReactNode;
+}
+
+interface DropdownMenuTriggerProps {
+  children: React.ReactNode;
+  asChild?: boolean;
+}
+
+interface DropdownMenuContentProps {
+  children: React.ReactNode;
+  align?: "start" | "end";
+}
+
+interface DropdownMenuItemProps {
+  children: React.ReactNode;
+  onClick?: () => void;
+}
+
+export function DropdownMenu({ children }: DropdownMenuProps) {
+  return <div className="relative">{children}</div>;
+}
+
+export function DropdownMenuTrigger({
+  children,
+  asChild,
+}: DropdownMenuTriggerProps) {
+  return (
+    <div className={classNames({ "inline-block": asChild })}>{children}</div>
+  );
+}
+
+export function DropdownMenuContent({
+  children,
+  align = "start",
+}: DropdownMenuContentProps) {
+  const alignClasses = align === "end" ? "right-0" : "left-0";
+  return (
+    <div className={`absolute ${alignClasses} mt-2 w-48 bg-white shadow-lg`}>
+      {children}
+    </div>
+  );
+}
+
+export function DropdownMenuItem({ children, onClick }: DropdownMenuItemProps) {
+  return (
+    <div
+      className="px-4 py-2 hover:bg-gray-200 cursor-pointer"
+      onClick={onClick}
+    >
+      {children}
+    </div>
+  );
+}
