f9b9f9d5d2206f26bd5f81b1d9233d0a4bd23dd5
Ebrahim Beiati-Asl <ebrahimbeiaty@gmail.com>
2024-05-24 13:21:59 +0100

    edit

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,29 @@
 import Hero from "@/components/Hero";
 import { ModeToggle } from "@/components/mode-toggle";
+import { IoHomeOutline } from "react-icons/io5";
+import { FloatingNav } from '@/components/ui/FloatingNav';
+import Grid from '@/components/Grid';
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="relative bg-black flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5 ">
+      {" "}
+      <ModeToggle />
       <div className="max-w-7xl w-full">
+        <FloatingNav
+          navItems={[
+            {
+              name: "Home",
+              link: "/",
+              icon: (
+                <IoHomeOutline className="h-4 w-4 text-neutral-500 dark:text-white" />
+              ),
+            },
+          ]}
+        />
         <Hero />
-        <ModeToggle/>
+        <Grid/>
       </div>
     </main>
   );
--- components/mode-toggle.tsx
+++ components/mode-toggle.tsx
@@ -12,14 +12,13 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 
-// ModeToggle Component
 export function ModeToggle() {
   const { setTheme } = useTheme();
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon" className="fixed top-4 right-4">
+        <Button variant="outline" size="icon">
           <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
